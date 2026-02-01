be7d5549c4ec0753745f6f29f610a25559021108
BlissfulAlloy79 <xutony2006@gmail.com>
2026-01-31 23:44:13 -0500

    udpated main.py to parse all data

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from pwn import remote
+import json
 
 p = remote("scripting.ctf.pascalctf.it", 6004)
+
+bomb_info: dict = {}
+p.recvuntil(b"Serial Number: ")
+bomb_info["Serial Number"] = p.recvline().decode().strip()
+
+p.recvuntil(b"Batteries: ")
+bomb_info["Batteries"] = p.recvline().decode().strip()
+
+p.recvuntil(b"Label: ")
+bomb_info["Label"] = p.recvline().decode().strip()
+
+p.recvuntil(b"Ports: ")
+bomb_info["Ports"] = p.recvline().decode().strip().split(", ")
+
+print(bomb_info)
 
 p.sendline(b"")
 
@@ -8,20 +24,25 @@ p.recvuntil(b"Module: ")
 module = p.recvline().decode().strip()
 print(module)
 
+p.recvuntil(b"Data: ")
+response = p.recvline().decode().strip()
+r_dict = eval(response)  # convert to dict
+print(r_dict)
+
 match module:
     case "Keypads":
-        print("it is keypads!")
+        print("it is Keypads")
     case "Complicated Wires":
-        print("it is complicated wires")
+        print("it is Complicated Wires")
     case "Wires":
-        print("it is wires")
+        print("it is Wires")
     case "Button":
-        print("it is button")
+        print("it is Button")
     case _:
         print(f"something new: {module}")
 
-p.recvuntil(b"Data: ")
-response = p.recvline().decode().strip()
-print(response)
+# write data to the io
 
 p.close()
+
+# def button(data: dict) -> None:
