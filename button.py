3537c0fb026169b28d86350d0e8d370cac0b4857
tanaidan <aidan.tan@mail.utoronto.ca>
2026-02-01 00:33:41 -0500

    Completed button.py

--- button.py
+++ button.py
@@ -7,21 +7,38 @@ def solve(data: dict, info: dict) -> list:
     indicator = info['Label']
     
     if data['color'] == 'Blue' and data['text'] == 'Abort':
-        release_held_button(solution, data['strip_color'])
+        # 1
+        release_held_button(solution, data['color_strip'])
     elif batteries > 1 and data['text'] == 'Detonate':
-        pass    
+        # 2
+        solution.append('1')
+    elif data['color'] == 'White' and indicator == 'CAR':
+        # 3
+        release_held_button(solution, data['color_strip'])
+    elif batteries > 2 and indicator == 'FRK':
+        # 4
+        solution.append('1')
+    elif data['color'] == 'Yellow':
+        # 5
+        release_held_button(solution, data['color_strip'])
+    elif data['color'] == 'Red' and data['text'] == 'Hold':
+        # 6
+        solution.append('1')
+    else:
+        release_held_button(solution, data['color_strip'])
+        
     return solution
 
 
 def release_held_button(solution: list, strip_color: str):
     solution.append('2')
-    if strip_color == 'Blue':
-        solution.append('4')
-    elif strip_color == 'White':
-        solution.append('1')
-    elif strip_color == 'Yellow':
-        solution.append('5')
-    else:
-        solution.append('1')
-        
+    match (strip_color):
+        case 'Blue':
+            solution.append('4')
+        case 'White':
+            solution.append('1')
+        case 'Yellow':
+            solution.append('5')
+        case _:
+            solution.append('1')
             
