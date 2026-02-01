98fb0222cc908214816d6c92e874b6613e01b6f1
BlissfulAlloy79 <xutony2006@gmail.com>
2026-02-01 01:59:50 -0500

    merged button

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pwn import remote
 from keypads import Keypads
 from complicated_wires import CWire
+from button import Button
 
 
 def write_output(p, answer_sequence):
@@ -9,7 +10,46 @@ def write_output(p, answer_sequence):
         p.sendline(i.encode())
 
 
-def read_solve_cycle(p) -> None:
+def read_solve_cycle(p, bomb_info) -> None:
+    p.recvuntil(b"Module: ")
+    module = p.recvline().decode().strip()
+    print(module)
+
+    p.recvuntil(b"Data: ")
+    response = p.recvline().decode().strip()
+    data = eval(response)  # convert to dict
+    print(data)
+
+    match module:
+        case "Keypads":
+            # print("it is Keypads")
+            keypads = Keypads()
+            answer_sequence = keypads.compute_order(data)
+            print(answer_sequence)
+            write_output(p, answer_sequence)
+        case "Complicated Wires":
+            # print("it is Complicated Wires")
+            c_wire = CWire()
+            answer_sequence = c_wire.solve(data, bomb_info)
+            print(answer_sequence)
+            write_output(p, answer_sequence)
+        case "Wires":
+            print("it is Wires")
+        case "Button":
+            # print("it is Button")
+            button = Button()
+            answer_sequence = button.solve(data, bomb_info)
+            write_output(p, answer_sequence)
+        case _:
+            print(f"something new: {module}")
+
+    p.sendline(b"")
+    # p.interactive()
+
+
+def main() -> None:
+    p = remote("scripting.ctf.pascalctf.it", 6004)
+
     bomb_info: dict = {}
     p.recvuntil(b"Serial Number: ")
     bomb_info["Serial Number"] = p.recvline().decode().strip()
@@ -27,42 +67,10 @@ def read_solve_cycle(p) -> None:
 
     p.sendline(b"")
 
-    p.recvuntil(b"Module: ")
-    module = p.recvline().decode().strip()
-    print(module)
+    for _ in range(100):
+        read_solve_cycle(p, bomb_info)
 
-    p.recvuntil(b"Data: ")
-    response = p.recvline().decode().strip()
-    data = eval(response)  # convert to dict
-    print(data)
-
-    match module:
-        case "Keypads":
-            print("it is Keypads")
-            keypads = Keypads()
-            answer_sequence = keypads.compute_order(data)
-            print(answer_sequence)
-            write_output(p, answer_sequence)
-        case "Complicated Wires":
-            print("it is Complicated Wires")
-            c_wire = CWire()
-            answer_sequence = c_wire.solve(data, bomb_info)
-            print(answer_sequence)
-            write_output(p, answer_sequence)
-        case "Wires":
-            print("it is Wires")
-        case "Button":
-            print("it is Button")
-        case _:
-            print(f"something new: {module}")
-
-    # p.sendline(b"")
     p.interactive()
-
-
-def main() -> None:
-    p = remote("scripting.ctf.pascalctf.it", 6004)
-    read_solve_cycle(p)
 
 
 if __name__ == "__main__":
